a0141f4cb8e26a5d8890cb4a4f021c8dc070217a
kputnins <kaspars.putnins19@gmail.com>
2024-10-12 14:21:52 +0300

    refactor: moved modal logic to scripts

--- src/main.ts
+++ src/main.ts
@@ -36,34 +36,28 @@ type GameState = {
 };
 
 class Game {
-  private canvas: Canvas;
-  private controls: Controls;
-  private state: GameState;
+  private canvas!: Canvas;
+  private controls!: Controls;
+  private state!: GameState;
 
   constructor() {
-    const canvas = document.getElementById(
-      "canvas",
-    ) as HTMLCanvasElement | null;
-    if (!canvas) {
-      throw new Error("Failed to create canvas element.");
-    }
+    this.initControls();
+    this.initCanvas();
+    this.initState();
+  }
 
-    const context = canvas.getContext("2d");
-    if (!context) {
-      console.log("Canvas element", canvas);
-      throw new Error("Failed to get 2D context.");
-    }
-
-    this.canvas = { htmlElement: canvas, context: context };
-
-    const panel = document.getElementById("controls") as HTMLDivElement;
+  initControls() {
+    const panel = document.getElementById("controls") as HTMLDivElement | null;
     const newGameButton = document.getElementById(
-      "new-game",
-    ) as HTMLButtonElement;
+      "new-game-button",
+    ) as HTMLButtonElement | null;
     const optionsButton = document.getElementById(
-      "options",
-    ) as HTMLButtonElement;
-    const infoButton = document.getElementById("info") as HTMLButtonElement;
+      "options-button",
+    ) as HTMLButtonElement | null;
+    const infoButton = document.getElementById(
+      "info-button",
+    ) as HTMLButtonElement | null;
+
     if (!panel || !newGameButton || !optionsButton || !infoButton) {
       console.log("Controls element", panel);
       console.log("New game button", newGameButton);
@@ -79,6 +73,66 @@ class Game {
       infoButton: infoButton,
     };
 
+    this.initInfoDialog();
+  }
+
+  initInfoDialog() {
+    const dialog = document.getElementById(
+      "info-dialog",
+    ) as HTMLDialogElement | null;
+    const showButton = document.getElementById(
+      "info-button",
+    ) as HTMLButtonElement | null;
+    const closeButton = dialog?.querySelector(
+      "dialog button",
+    ) as HTMLButtonElement | null;
+
+    if (!dialog || !showButton || !closeButton) {
+      console.log("Info dialog", dialog);
+      console.log("Info button", showButton);
+      console.log("Close button", closeButton);
+      throw new Error("Failed to get info dialog elements.");
+    }
+
+    showButton.addEventListener("click", () => {
+      dialog.showModal();
+    });
+
+    closeButton.addEventListener("click", () => {
+      dialog.close();
+    });
+  }
+
+  initCanvas() {
+    const canvas = document.getElementById(
+      "canvas",
+    ) as HTMLCanvasElement | null;
+    if (!canvas) {
+      throw new Error("Failed to create canvas element.");
+    }
+
+    const context = canvas.getContext("2d");
+    if (!context) {
+      console.log("Canvas element", canvas);
+      throw new Error("Failed to get 2D context.");
+    }
+
+    this.canvas = { htmlElement: canvas, context: context };
+
+    window.addEventListener("resize", this.handleResize.bind(this));
+    this.handleResize();
+  }
+
+  handleResize() {
+    const MAGIX_MISSING_PIXELS = 6;
+    this.canvas.htmlElement.width = window.innerWidth;
+    this.canvas.htmlElement.height =
+      window.innerHeight -
+      this.controls.panel.clientHeight -
+      MAGIX_MISSING_PIXELS;
+  }
+
+  initState() {
     this.state = {
       tick: 0,
       player: {
@@ -92,29 +146,12 @@ class Game {
         sprite: new Image(),
       },
     };
-
-    this.handleResize();
-    window.addEventListener("resize", this.handleResize.bind(this));
-  }
-
-  handleResize() {
-    console.log(
-      "Resizing canvas...",
-      window.innerHeight,
-      this.controls.panel.clientHeight,
-    );
-    this.canvas.htmlElement.width = window.innerWidth;
-    this.canvas.htmlElement.height =
-      window.innerHeight - this.controls.panel.clientHeight - 6;
   }
 }
 
 function main() {
   const game = new Game();
-
   window.game = game;
-
-  console.log("Game initialized.", game.state);
 }
 
 main();
